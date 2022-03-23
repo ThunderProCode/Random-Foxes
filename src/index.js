c61658161474fa750993c6d144a071d0136c846e
Hector Eddin Acosta Najera <hector889ju@gmail.com>
2022-03-23 12:53:11 -0600

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,24 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
-
 console.log('Happy hacking :)')
+
+const imagesNode = document.getElementById('images');
+const addButton = document.getElementById('add-image');
+
+const baseUrl = "https://randomfox.ca/images/";
+
+addButton.addEventListener("click",addImage);
+
+const createImageNode = () => {
+    const imageContainer = document.createElement('div');
+    imageContainer.className = "p-4";
+    const randomInt = Math.floor(Math.random() * 123)+1;
+    const image = document.createElement('img');
+    image.className = "mx-auto";
+    image.width = "320";
+    image.src = `${baseUrl}${randomInt}.jpg`;
+    imageContainer.append(image);
+    return imageContainer;
+}
+
+function addImage() {
+    imagesNode.appendChild(createImageNode());
+}
